71b86af5068f6caa8c368134f5d1f26b49f2db0b
realdinho <realdo.dias3@gmail.com>
2023-03-28 02:56:40 +0800

    add Angular client and implement get users API

--- API/Program.cs
+++ API/Program.cs
@@ -11,9 +11,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+// CORS
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
