268fdb56113e1b47a0da5298191d9c72962b15e9
realdinho <realdo.dias3@gmail.com>
2023-04-11 19:11:48 +0800

    add tabs in the member details component

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToastrModule } from 'ngx-toastr';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @NgModule({
   declarations: [],
@@ -11,10 +12,12 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-center'
     }),
+    TabsModule.forRoot(),
   ],
   exports: [
     BsDropdownModule,
     ToastrModule,
+    TabsModule,
   ]
 })
 export class SharedModule { }
