93c21c76eb5cda808a8a4615e934b9864a561669
realdinho <realdo.dias3@gmail.com>
2023-04-11 16:56:09 +0800

    create member service

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Member } from '../_models/member';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'users', this.getHttpOptions());
+  }
+
+  getMember(username: string) {
+    return this.http.get<Member>(this.baseUrl + 'users/' + username, this.getHttpOptions())
+  }
+
+  getHttpOptions() {
+    const userStr = localStorage.getItem('user');
+    if (!userStr) return;
+    const user = JSON.parse(userStr);
+    return {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + user.token
+      })
+    }
+  }
+}
