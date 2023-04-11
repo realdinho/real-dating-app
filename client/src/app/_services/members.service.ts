637c220a83a3c788b75be8fa5b13cfc1ee252fd9
realdinho <realdo.dias3@gmail.com>
2023-04-11 18:11:49 +0800

    add interceptor for user token bearer

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -14,21 +14,10 @@ export class MembersService {
   ) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users', this.getHttpOptions());
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, this.getHttpOptions())
-  }
-
-  getHttpOptions() {
-    const userStr = localStorage.getItem('user');
-    if (!userStr) return;
-    const user = JSON.parse(userStr);
-    return {
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + user.token
-      })
-    }
+    return this.http.get<Member>(this.baseUrl + 'users/' + username)
   }
 }
