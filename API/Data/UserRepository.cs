4121319f7df56dc1db051827c9a1f1e062fada53
realdinho <realdo.dias3@gmail.com>
2023-04-21 19:27:48 +0800

    add filters by username, age, and gender

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -29,11 +29,21 @@ namespace API.Data
 
         public async Task<PagedList<MemberDTO>> GetMembersAsync(UserParams userParams)
         {
-            var query = _context.Users
-                .ProjectTo<MemberDTO>(_mapper.ConfigurationProvider)
-                .AsNoTracking();
+            var query = _context.Users.AsQueryable();
 
-            return await PagedList<MemberDTO>.CreateAsync(query, userParams.PageNumber, userParams.PageSize);
+            query = query.Where(m => m.UserName != userParams.CurrentUsername);
+            query = query.Where(m => m.Gender == userParams.Gender);
+
+            var minDOB = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MaxAge - 1));
+            var maxDOB = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MinAge));
+
+            query = query.Where(u => u.DateOfBirth >= minDOB && u.DateOfBirth <= maxDOB);
+
+            return await PagedList<MemberDTO>.CreateAsync(
+                query.AsNoTracking().ProjectTo<MemberDTO>(_mapper.ConfigurationProvider), 
+                userParams.PageNumber, 
+                userParams.PageSize
+            );
         }
 
         public async Task<AppUser> GetUserByIdAsync(int id)
