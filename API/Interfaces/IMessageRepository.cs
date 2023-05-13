f1e5a882c9c25fc61cfe17d9a5f39dbb84d803dd
realdinho <realdo.dias3@gmail.com>
2023-05-13 20:14:28 +0800

    add connection and group endpoints

--- API/Interfaces/IMessageRepository.cs
+++ API/Interfaces/IMessageRepository.cs
@@ -12,5 +12,9 @@ namespace API.Interfaces
         Task<PagedList<MessageDTO>> GetMessagesForUser(MessageParams messageParams);
         Task<IEnumerable<MessageDTO>> GetMessageThread(string currentUsername, string recipientUsername);
         Task<bool> SaveAllAsync();
+        void AddGroup(Group group);
+        void RemoveConnection(Connection connection);
+        Task<Connection> GetConnection(string connectionId);
+        Task<Group> GetMessageGroup(string groupName);
     }
 }
