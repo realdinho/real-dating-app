0e557a481227bd6145618a49e20d853ec5317748
realdinho <realdo.dias3@gmail.com>
2023-05-18 17:35:53 +0800

    pre-deploymnet

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 import { map, of, take } from 'rxjs';
-import { PaginatedResult } from '../_models/pagination';
 import { UserParams } from '../_models/userParams';
 import { AccountService } from './account.service';
 import { User } from '../_models/user';
